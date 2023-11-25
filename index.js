bac08823dcc19aed8909da2c6282c53885811856
AndyTechPro <chukwuandrew2025@gmail.com>
2023-11-25 23:52:59 +0100

    fift commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ require("dotenv").config();
 const salt = bcrypt.genSaltSync(10);
 const secret = 'mywebblogsecret';
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'https://ninejaback.onrender.com' }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
