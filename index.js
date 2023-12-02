a293b486aea95e4c871751b7e9e9c0d3b70b29b0
AndyTechPro <chukwuandrew2025@gmail.com>
2023-12-02 20:34:46 +0100

    fix 10

--- index.js
+++ index.js
@@ -431,15 +431,16 @@ app.get('/related-posts', async (req, res) => {
       });
   
       if (results.length === 0) {
-        res.json({ results: [], message: 'No results found' });
+        res.status(404).json({ results: [], message: 'No results found' });
       } else {
-        res.json({ results });
+        res.status(200).json({ results });
       }
     } catch (error) {
       console.error('Error during search:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
+  
   
   
   
